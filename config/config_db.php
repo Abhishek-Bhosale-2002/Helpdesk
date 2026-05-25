dd1f08e9628510d4ec7a83a6be32dcddd99440ef
Abhishek Bhosale <abhishekbhosale661@gmail.com>
2026-05-25 16:44:34 +0530

    Add files via upload

---
+++ config/config_db.php
@@ -0,0 +1,12 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost = 'localhost';
+   public $dbuser = 'glpi-owner';
+   public $dbpassword = 'glpi123%24%25';
+   public $dbdefault = 'glpi';
+   public $use_utf8mb4 = true;
+   public $allow_myisam = false;
+   public $allow_datetime = false;
+   public $allow_signed_keys = false;
+   public $use_timezones = true;
+}
